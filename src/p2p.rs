d41f80e65a1609fdeb66b65b0e5d805578be5f76
AlcachofaK_Dev <jonnyma1011@gmail.com>
2025-09-18 22:13:01 -0600

    Create p2p.rs

---
+++ src/p2p.rs
@@ -0,0 +1,46 @@
+use std::{error::Error, time::Duration, u64};
+
+use futures::prelude::*;
+use libp2p::{noise, ping, swarm::SwarmEvent, tcp, yamux, Multiaddr};
+use tokio::sync::mpsc;
+use tracing_subscriber::EnvFilter;
+
+pub struct P2pNode {
+    pub local_addr: Option<Multiaddr>,
+}
+
+pub async fn init_node(
+    tx: mpsc::Sender<Multiaddr>,
+) -> Result<Swarm<ping::Behaviour>, Box<dyn Error>> {
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?
+        .with_behaviour(|_| ping::Behaviour::default())?
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX)))
+        .build();
+
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    tokio::spawn(async move{
+        loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr { address, .. } => {
+                let _ = tx.send(address).await;
+            }
+            SwarmEvent::Behaviour(event) => println!("{event:?}"),
+            _ => {}
+        }
+    }
+    });
+
+    Ok(swarm)
+}
+pub fn dial(swarm: &mut Swarm<ping::Behaviour>, addr: &str) -> Result<(), Box<dyn Error>> {
+        let remote: Multiaddr = addr.parse()?;
+        swarm.dial(remote)?;
+        Ok(())
+    }
